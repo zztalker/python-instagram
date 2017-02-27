9702c7f1194848fefef86ebd8061b3160cac019b
wkoot <github@rondarchief.nl>
2017-02-27 15:42:55 +0100

    Revert previous attempt; try within the class

--- tests.py
+++ tests.py
@@ -64,9 +64,11 @@ class TestInstagramAPI(client.InstagramAPI):
         return actual_val
 
 
-@unittest.skipUnless(TEST_AUTH, 'Skipping InstagramAuthTests')
 class InstagramAuthTests(unittest.TestCase):
     def setUp(self):
+        if not TEST_AUTH:
+            raise unittest.SkipTest()
+
         self.unauthenticated_api = TestInstagramAPI(client_id=client_id, redirect_uri=redirect_uri, client_secret=client_secret)
 
     def test_authorize_login_url(self):
